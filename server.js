4b969e5203939ddb59f56ab509d42e9e43f216fd
miguelrohv <miguelrohv@gmail.com>
2018-10-26 00:35:05 +0800

    setup of the ejs pages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const app = express();
 const bodyParser = require('body-parser');
-const mysql = require('mysql'); 
+app.set('view engine', 'ejs');
 //port used if none given
 const port = process.env.port || 3000;
 //parsing body for params , body ,etc.
@@ -11,6 +11,11 @@ app.use(bodyParser.json());
 //route specified
 const items = require('./api/routes/items');
 app.use('/items',items);
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
 //app listen port
 app.listen(port, function () {
     console.log('Node app is running on port:'+port);
