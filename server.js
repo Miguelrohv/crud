4d214d6d461de9662eadee7544eed32cb5181463
miguelrohv <miguelrohv@gmail.com>
2018-10-25 21:38:46 +0800

    setup body parser

--- server.js
+++ server.js
@@ -2,5 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql'); 
-
+//port used if none given
 const port = process.env.port || 3000;
+//route specified
+const items = require('./api/routes/items');
+app.use('/items',items);
+//parsing body for params , body ,etc.
+app.use(bodyParser.urlencoded({extended:false}));
+//extracts json data
+app.use(bodyParser.json());
